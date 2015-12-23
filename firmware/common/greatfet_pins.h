44a4424695650b90485a153daacd501e4ebcbb88
Dominic Spill <dominicgs@gmail.com>
2015-12-23 21:30:01 +0000

    Add GreatFET pin definitions header

---
+++ firmware/common/greatfet_pins.h
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2015 Dominic Spill <dominicgs@gmail.com>
+ *
+ * This file is part of GreatFET.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __GREATFET_PINS_H
+#define __GREATFET_PINS_H
+
+/*
+ * SCU PinMux
+ */
+
+/* GPIO Output PinMux */
+
+#define SCU_PINMUX_LED1     (P7_6)  /* GPIO3[14] on P7_6 */
+#define SCU_PINMUX_LED2     (P4_1)  /* GPIO2[1] on P4_1 */
+#define SCU_PINMUX_LED3     (P7_5)  /* GPIO3[13] on P7_5 */
+#define SCU_PINMUX_LED4     (P7_4)  /* GPIO3[12] on P7_4 */
+
+#define PIN_LED1            (14) /* GPIO3[14] on P7_6 */
+#define PIN_LED2            (1)  /* GPIO2[1] on P4_1 */
+#define PIN_LED3            (13) /* GPIO3[13] on P7_5 */
+#define PIN_LED4            (12) /* GPIO3[12] on P7_4 */
+
+#define PORT_LED1_3_4       (3) /* PORT for LED1, 3, 4 */
+#define PORT_LED2           (2) /* PORT for LED1, 3, 4 */
+
+#define SCU_PINMUX_EN1V8    (P6_10) /* GPIO3[6] on P6_10 */
+
+/* GPIO Input PinMux */
+#define SCU_PINMUX_BOOT0    (P1_1)  /* GPIO0[8] on P1_1 */
+#define SCU_PINMUX_BOOT1    (P1_2)  /* GPIO0[9] on P1_2 */
+#define SCU_PINMUX_BOOT2    (P2_8)  /* GPIO5[7] on P2_8 */
+#define SCU_PINMUX_BOOT3    (P2_9)  /* GPIO1[10] on P2_9 */
+
+/* SSP1 Peripheral PinMux */
+#define SCU_SSP1_MISO       (P1_3)  /* P1_3 */
+#define SCU_SSP1_MOSI       (P1_4)  /* P1_4 */
+#define SCU_SSP1_SCK        (P1_19) /* P1_19 */
+#define SCU_SSP1_SSEL       (P1_20) /* P1_20 */
+
+/* JTAG interface */
+#define SCU_PINMUX_TDO (P9_5)  /* GPIO5[18] */
+#define SCU_PINMUX_TCK (P6_1)  /* GPIO3[ 0] */
+#define SCU_PINMUX_TMS (P6_5)  /* GPIO3[ 4] */
+#define SCU_PINMUX_TDI (P6_2)  /* GPIO3[ 1] */
+
+
+/* CPLD SGPIO interface */
+#define SCU_PINMUX_SGPIO0   (P0_0)
+#define SCU_PINMUX_SGPIO1   (P0_1)
+#define SCU_PINMUX_SGPIO2   (P1_15)
+#define SCU_PINMUX_SGPIO3   (P1_16)
+#define SCU_PINMUX_SGPIO4   (P6_3)
+#define SCU_PINMUX_SGPIO5   (P6_6)
+#define SCU_PINMUX_SGPIO6   (P2_2)
+#define SCU_PINMUX_SGPIO7   (P1_0)
+#define SCU_PINMUX_SGPIO8   (P9_6)
+#define SCU_PINMUX_SGPIO9   (P4_3)
+#define SCU_PINMUX_SGPIO10  (P1_14)
+#define SCU_PINMUX_SGPIO11  (P1_17)
+#define SCU_PINMUX_SGPIO12  (P1_18)
+#define SCU_PINMUX_SGPIO13  (P4_8)
+#define SCU_PINMUX_SGPIO14  (P4_9)
+#define SCU_PINMUX_SGPIO15  (P4_10)
+
+/* SPI flash */
+#define SCU_SSP0_MISO       (P3_6)
+#define SCU_SSP0_MOSI       (P3_7)
+#define SCU_SSP0_SCK        (P3_3)
+#define SCU_SSP0_SSEL       (P3_8) /* GPIO5[11] on P3_8 */
+#define SCU_FLASH_HOLD      (P3_4) /* GPIO1[14] on P3_4 */
+#define SCU_FLASH_WP        (P3_5) /* GPIO1[15] on P3_5 */
+
+/* TODO add other Pins */
+#define SCU_PINMUX_GPIO3_8  (P7_0)  /* GPIO3[8] */
+#define SCU_PINMUX_GPIO3_9  (P7_1)  /* GPIO3[9] */
+#define SCU_PINMUX_GPIO3_10 (P7_2)  /* GPIO3[10] */
+#define SCU_PINMUX_GPIO3_11 (P7_3)  /* GPIO3[11] */
+#define SCU_PINMUX_GPIO3_12 (P7_4)  /* GPIO3[12] */
+#define SCU_PINMUX_GPIO3_13 (P7_5)  /* GPIO3[13] */
+#define SCU_PINMUX_GPIO3_14 (P7_6)  /* GPIO3[14] */
+#define SCU_PINMUX_GPIO3_15 (P7_7)  /* GPIO3[15] */
+
+#define SCU_PINMUX_SD_POW   (P1_5)  /* GPIO1[8] */
+#define SCU_PINMUX_SD_CMD   (P1_6)  /* GPIO1[9] */
+#define SCU_PINMUX_SD_VOLT0 (P1_8)  /* GPIO1[1] */
+#define SCU_PINMUX_SD_DAT0  (P1_9)  /* GPIO1[2] */
+#define SCU_PINMUX_SD_DAT1  (P1_10) /* GPIO1[3] */
+#define SCU_PINMUX_SD_DAT2  (P1_11) /* GPIO1[4] */
+#define SCU_PINMUX_SD_DAT3  (P1_12) /* GPIO1[5] */
+#define SCU_PINMUX_SD_CD    (P1_13) /* GPIO1[6] */
+
+#define SCU_PINMUX_U0_TXD   (P2_0)  /* GPIO5[0] */
+#define SCU_PINMUX_U0_RXD   (P2_1)  /* GPIO5[1] */
+
+#define SCU_PINMUX_ISP      (P2_7)  /* GPIO0[7] */
+
+#define SCU_PINMUX_GP_CLKIN	(P4_7)
+
+#endif /* __GREATFET_PINS_H */
