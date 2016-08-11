0dd3172e67ac92985dec39607b1f47c13111b5f2
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2016-08-10 19:11:04 -0700

    No more whitespace for greatfet host libraries

--- host/greatfet/peripherals/spi_flash.py
+++ host/greatfet/peripherals/spi_flash.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2016 Kyle J. Temkin <kyle@ktemkin.com>
 # All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without 
+# Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
 # 1. Redistributions of source code must retain the above copyright notice,
@@ -269,6 +269,3 @@ class SPIFlash(object):
             value=address_high, index=address_low, length=length)
 
         return result
-
-
-
