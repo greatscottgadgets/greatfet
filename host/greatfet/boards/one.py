7bf0ba89a74b555555e43c0a38780d8f5fe69fcf
Dominic Spill <dominicgs@gmail.com>
2016-11-01 20:25:36 -0600

    Rename Azalea to GreatFET One

---
+++ host/greatfet/boards/one.py
@@ -0,0 +1,57 @@
+#
+# Copyright (c) 2016 Kyle J. Temkin <kyle@ktemkin.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its contributors
+#    may be used to endorse or promote products derived from this software
+#    without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+#
+
+from ..board import GreatFETBoard
+from ..peripherals.spi_flash import SPIFlash
+from ..peripherals.i2c_bus import I2CBus
+from ..peripherals import gpio
+
+class GreatFETOne(GreatFETBoard):
+    """ Class representing GreatFET One base-boards. """
+
+    # Currently, all GreatFET One boards have an ID of zero.
+    HANDLED_BOARD_IDS = [0]
+    BOARD_NAME = "GreatFET One"
+
+
+    def __init__(self, **device_identifiers):
+        """ Initialize a new GreatFET One connection. """
+
+        # Set up the core connection.
+        super(GreatFETOne, self).__init__(**device_identifiers)
+
+        # Initialize the fixed peripherals that come on the board.
+        # TODO: Use a self.add_peripheral mechanism, so peripherals can
+        # be dynamically listed?
+        self.onboard_flash = SPIFlash(self)
+        self.i2c_busses = [ I2CBus(self, 'I2C0') ]
+
+        # Create an easy-to-use alias for the primary I2C bus, for rapid
+        # hacking/experimentation.
+        self.i2c = self.i2c_busses[0]
