80324c822a2443c6740ba909f5a4abe3aa4f0337
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2016-11-30 21:01:51 -0700

    Rewrite the PCA9674 code to use the new IOExpander interface

--- host/greatfet/io_expanders/pca9674.py
+++ host/greatfet/io_expanders/pca9674.py
@@ -31,7 +31,7 @@
 from ..io_expander import DIOExpander
 from ..peripherals.i2c_device import I2CDevice
 
-class PCA9674(I2CDevice, DIOExpander):
+class PCA9674(DIOExpander, I2CDevice):
     """PCA9674 I²C I/O expander"""
 
     def __init__(self, bus, address=0x20):
@@ -42,20 +42,42 @@ class PCA9674(I2CDevice, DIOExpander):
                 GreatFET One, this would typically be (board.i2c).
             address: Optional; used to specify a non-default address.
         """
+        super(PCA9674, self).__init__()
+        I2CDevice.__init__(self, bus, address, 'PCA9674')
 
-        super(PCA9674, self).__init__(bus, address, 'PCA9674')
+        # Power up value of pins with the PCA9674
+        self._pins = 0xFF
 
     @property
     def number_pins(self):
         return 8
 
+
     def set_direction(self, value):
+        value = self._validate_port_value(value)
+
         # Invert value
         pin_states = ~value & 2**self.number_pins - 1
+
         self.transmit(pin_states, 0)
+        self._directions = pin_states
+
+    def set_pin_direction(self, pin_num, direction):
+        super(PCA9674, self).set_pin_direction(pin_num, direction)
+
 
     def read(self):
         return self.transmit([], 1)[0]
 
+    def read_pin(self, pin_num):
+        return super(PCA9674, self).read_pin(pin_num)
+
+
     def write(self, value):
-        self.transmit([self._validate_pin_values(value)])
+        value = self._validate_port_value(value)
+
+        self.transmit([value], 0)
+        self._pins = value
+
+    def write_pin(self, pin_num, value):
+        super(PCA9674, self).write_pin(pin_num, value)
