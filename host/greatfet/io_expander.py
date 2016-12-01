5848815e3b8f55dbb63c2cbc5e82d601a3e9f5c9
Schuyler St. Leger <schuyler.st.leger@gmail.com>
2016-11-30 19:18:45 -0700

    Make new and improved IOExpander API

--- host/greatfet/io_expander.py
+++ host/greatfet/io_expander.py
@@ -36,10 +36,16 @@ class DIOExpander(object):
     """
     __metaclass__ = ABCMeta
 
+    @abstractmethod
+    def __init__(self):
+        self._pins = 0
+        self._directions = 0
+
     @abstractproperty
     def number_pins(self):
         """Return the number of expanded pins as an integer."""
         return 0
+
 
     @abstractmethod
     def set_direction(self, directions):
@@ -47,20 +53,91 @@ class DIOExpander(object):
         0 represents an input
         1 represents an output
         """
-        return
+        directions = self._validate_port_value(directions)
+        write_directions = directions
+
+        for i in range(self.number_pins):
+            self.set_pin_direction(i, directions & 1)
+            directions = (directions >> 1)
+
+        self._directions = write_directions
+
+    @abstractmethod
+    def set_pin_direction(self, pin_num, direction):
+        pin_num, direction = self._validate_pin_args(pin_num, direction)
+
+        directions = self._set_bit(self._directions, pin_num, direction)
+
+        self.set_direction(directions)
+        self._directions = directions
+
 
     @abstractmethod
     def read(self):
         """Return the value of the pins."""
-        return
+        pin_values = 0
+        for i in range(self.number_pins - 1, -1, -1):
+            pin_values = (pin_values << 1) | self.read_pin(i)
+
+        return pin_values
+
+    @abstractmethod
+    def read_pin(self, pin_num):
+        pin_num = self._validate_pin_number(pin_num)
+
+        return self._git_bit(self.read(), pin_num)
+
 
     @abstractmethod
     def write(self, value):
         """Set the value of the pins."""
-        return
+        value = self._validate_port_value(value)
+        write_value = value
 
-    def _validate_pin_values(self, value):
-        """Ensure the value is in a range that can represent the states of the pins."""
+        for i in range(self.number_pins):
+            self.write_pin(i, value & 1)
+            value = (value >> 1)
+
+        self._pins = write_value
+
+    @abstractmethod
+    def write_pin(self, pin_num, value):
+        pin_num, value = self._validate_pin_args(pin_num, value)
+        write_value = self._set_bit(self._pins, pin_num, value)
+
+        self.write(write_value)
+        self._pins = write_value
+
+
+    def _validate_pin_number(self, pin_num):
+        if pin_num < 0 or pin_num >= self.number_pins:
+            raise ValueError("Tried to access an invalid pin.")
+        return pin_num
+
+    @staticmethod
+    def _validate_pin_value(value):
+        value = int(value)
+        if value < 0 or value > 1:
+            raise ValueError("Tried to set an invalid value for a pin.")
+        return value
+
+    def _validate_port_value(self, value):
+        """Ensure the value is in a range that can represent the state of the pins."""
         if value < 0 or value > self.number_pins ** 2 - 1:
             raise ValueError("Tried to set an invalid value.")
         return value
+
+    def _validate_pin_args(self, pin_num, value):
+        return (self._validate_pin_number(pin_num), self._validate_pin_value(value))
+
+
+    @staticmethod
+    def _set_bit(bits, bit_num, bit_value):
+        if bit_value:
+            return bits | (1 << bit_num)
+        else:
+            return bits & ~(1 << bit_num)
+
+    @staticmethod
+    def _git_bit(bits, bit_num):
+        return (bits >> bit_num) & 1
