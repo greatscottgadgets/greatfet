af7a32f1c67f0ec13f3494b55459dd6f39b1f02c
Mike Naberezny <mike@naberezny.com>
2017-06-20 14:15:55 -0700

    Strip trailing whitespace

--- host/greatfet/peripherals/spi_bus.py
+++ host/greatfet/peripherals/spi_bus.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2017 Kyle J. Temkin <kyle@ktemkin.com>
 # All rights reserved.
 #
-# Redistribution and use in source and binary forms, with or without 
+# Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
 # 1. Redistributions of source code must retain the above copyright notice,
@@ -113,6 +113,3 @@ class SPIBus(GreatFETPeripheral):
             result = []
 
         return result
-
-
-
