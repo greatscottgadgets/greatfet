29db0f1cbd35e41d471b991649c7994adb24b3d0
Dominic Spill <dominicgs@gmail.com>
2017-08-28 18:08:11 -0600

    Change license on cmake files

--- firmware/cmake/greatfet-common.cmake
+++ firmware/cmake/greatfet-common.cmake
@@ -1,30 +1,6 @@
-# Copyright 2009 Uwe Hermann <uwe@hermann-uwe.de>
-# Copyright 2010 Piotr Esden-Tempski <piotr@esden.net>
-# Copyright 2012 Michael Ossmann <mike@ossmann.com>
-# Copyright 2012 Benjamin Vernoux <titanmkd@gmail.com>
-# Copyright 2012 Jared Boone <jared@sharebrained.com>
-# Copyright 2016 Dominic Spill <dominicgs@gmail.com>
-# Copyright 2017 K. J. Temkin <k@ktemkin.com>
 #
-# This file is part of GreatFET.
+# This file is part of GreatFET
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2, or (at your option)
-# any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; see the file COPYING.  If not, write to
-# the Free Software Foundation, Inc., 51 Franklin Street,
-# Boston, MA 02110-1301, USA.
-#
-
-# derived primarily from Makefiles in libopencm3
 
 enable_language(C CXX ASM)
 
