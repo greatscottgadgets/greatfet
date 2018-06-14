df801de75bbeb530859f49d6207b6058ca29037d
Dominic Spill <dominicgs@gmail.com>
2018-06-14 07:43:36 -0600

    Add some underlying jtag support

---
+++ firmware/common/jtag.c
@@ -0,0 +1,589 @@
+/*
+ * This file is part of GreatFET
+ * derived from GoodFET (thanks Travis)
+ */
+
+#include <libopencm3/lpc43xx/scu.h>
+#include <greatfet_core.h>
+#include <gpio.h>
+#include <gpio_lpc.h>
+#include <pins.h>
+#include "jtag.h"
+
+//JTAG commands
+#define JTAG_IR_SHIFT 0x80
+#define JTAG_DR_SHIFT 0x81
+#define JTAG_RESET_TAP 0x82
+#define JTAG_RESET_TARGET 0x83
+#define JTAG_DETECT_IR_WIDTH 0x84
+#define JTAG_DETECT_CHAIN_LENGTH 0x85
+#define JTAG_GET_DEVICE_ID 0x86
+#define JTAG_DR_SHIFT_MORE 0x87 // used for shiftings > 32bits.  assumes JTAG_DR_SHIFT with NOEND first
+//#define JTAG_DR_SHIFT20 0x91
+
+static struct gpio_t tdo = GPIO(0, 10);
+static struct gpio_t tdi = GPIO(0, 11);
+static struct gpio_t tms = GPIO(0, 15);
+static struct gpio_t tck = GPIO(1, 14);
+static struct gpio_t rst = GPIO(2, 3);
+static struct gpio_t tst = GPIO(2, 2);
+
+//! Set up the pins for JTAG mode.
+void jtag_setup(void)
+{
+	// TDO
+	scu_pinmux(SCU_PINMUX_GPIO0_10, SCU_GPIO_FAST | SCU_GPIO_PUP | SCU_CONF_FUNCTION0);
+	gpio_input(&tdo);
+	// TDI
+	scu_pinmux(SCU_PINMUX_GPIO0_11, SCU_GPIO_FAST | SCU_GPIO_PUP | SCU_CONF_FUNCTION0);
+	gpio_output(&tdi);
+	// TMS
+	scu_pinmux(SCU_PINMUX_GPIO0_15, SCU_GPIO_FAST | SCU_GPIO_PUP | SCU_CONF_FUNCTION0);
+	gpio_output(&tms);
+	// TCK
+	scu_pinmux(SCU_PINMUX_GPIO1_14, SCU_GPIO_FAST | SCU_GPIO_PUP | SCU_CONF_FUNCTION0);
+	gpio_output(&tck);
+	// !RST
+	scu_pinmux(SCU_PINMUX_GPIO2_3, SCU_GPIO_FAST | SCU_GPIO_PUP | SCU_CONF_FUNCTION0);
+	gpio_output(&rst);
+	// TST
+	scu_pinmux(SCU_PINMUX_GPIO2_2, SCU_GPIO_FAST | SCU_GPIO_PUP | SCU_CONF_FUNCTION0);
+	gpio_output(&tst);
+	jtag_state = UNKNOWN;
+}
+
+//! Stop JTAG, release pins
+void jtag_stop(void)
+{
+	;
+}
+
+#define SETTDI gpio_write(&tdi, 1)
+#define CLRTDI gpio_write(&tdi, 0)
+#define READTDO gpio_read(&tdo)
+#define SETTMS gpio_write(&tms, 1)
+#define CLRTMS gpio_write(&tms, 0)
+#define SETTCK gpio_write(&tck, 1)
+#define CLRTCK gpio_write(&tck, 0)
+
+#define SETTST gpio_write(&tst, 1)
+#define CLRTST gpio_write(&tst, 0)
+#define SETRST gpio_write(&rst, 1)
+#define CLRRST gpio_write(&rst, 0)
+
+uint8_t savedtclk;
+#define SAVETCLK savedtclk=gpio_read(&tdi)
+#define RESTORETCLK gpio_write(&tdi, savedtclk)
+
+//! JTAG device ID.
+unsigned char jtagid=0;
+
+//! Remembers what the current JTAG state is
+enum eTAPState jtag_state = UNKNOWN;
+
+//! Returns true if we're in any of the data register states
+int in_dr()
+{
+	return (int)(jtag_state & (SELECT_DR_SCAN | CAPTURE_DR |
+							   SHIFT_DR | EXIT1_DR | PAUSE_DR |
+							   EXIT2_DR | UPDATE_DR));
+}
+
+//! Returns true if we're in any of the instruction register states
+int in_ir()
+{
+	return (int)(jtag_state & (SELECT_IR_SCAN | CAPTURE_IR |
+							   SHIFT_IR | EXIT1_IR | PAUSE_IR |
+							   EXIT2_IR | UPDATE_IR));
+}
+
+//! Returns true if we're in run-test-idle state
+int in_run_test_idle()
+{
+	return (int)(jtag_state & RUN_TEST_IDLE);
+}
+
+//! Check the state
+int in_state(enum eTAPState state)
+{
+	return (int)(jtag_state & state);
+}
+
+
+//! Reset the target device
+void jtag_reset_target()
+{
+	SETRST;
+	led_toggle(LED4);
+	CLRRST;
+	led_toggle(LED4);
+}
+
+//! Clock the JTAG clock line
+void jtag_ticktock() 
+{
+	CLRTCK; 
+	led_toggle(LED4);
+	SETTCK; 
+	led_toggle(LED4);
+}
+
+//! Goes through test-logic-reset and ends in run-test-idle
+void jtag_reset_tap()
+{
+	CLRTDI;
+	SETTMS;
+	jtag_ticktock();
+	jtag_ticktock();
+	jtag_ticktock();
+	jtag_ticktock();
+	jtag_ticktock();  // now in Reset state
+	CLRTMS;
+	jtag_ticktock();  // now in Run-Test/Idle state
+	jtag_state = RUN_TEST_IDLE;
+}
+
+//! Get into Shift-IR or Shift-DR state
+void jtag_shift_register()
+{
+	// assumes we're in any state that can transition to Shift-IR or Shift-DR
+	if (!in_state(CAPTURE_DR | CAPTURE_IR | SHIFT_DR | 
+				  SHIFT_IR | EXIT2_DR | EXIT2_IR ))
+	{
+		// debugstr("Invalid JTAG state");
+		return;
+	}
+
+	CLRTDI;
+	CLRTMS;
+	jtag_ticktock();
+
+	if (in_dr())
+		jtag_state = SHIFT_DR;
+	else
+		jtag_state = SHIFT_IR;
+}
+
+//! Get into Capture-IR state
+void jtag_capture_ir()
+{
+	// assumes you're in Run-Test/Idle, Update-DR or Update-IR
+	if (!in_state(RUN_TEST_IDLE | UPDATE_DR | UPDATE_IR))
+	{
+		// debugstr("Invalid JTAG state");
+		return;
+	}
+
+	CLRTDI;
+	SETTMS;
+	jtag_ticktock(); // Select-DR-Scan
+	jtag_ticktock(); // Select-IR-Scan
+	CLRTMS;
+	jtag_ticktock(); // Capture-IR
+
+	jtag_state = CAPTURE_IR;
+}
+
+//! Get into Capture-DR state
+void jtag_capture_dr()
+{
+	// assumes you're in Run-Test/Idle, Update-DR or Update-IR
+	if (!in_state(RUN_TEST_IDLE | UPDATE_DR | UPDATE_IR))
+	{
+		// debugstr("Invalid JTAG state");
+		return;
+	}
+
+	CLRTDI;
+	SETTMS;
+	jtag_ticktock(); // Select-DR-Scan
+	CLRTMS;
+	jtag_ticktock(); // Capture-IR
+
+	jtag_state = CAPTURE_DR;
+}
+
+//! Gets back to run-test-idle without going through the test-logic-reset
+void jtag_run_test_idle()
+{
+	CLRTDI;
+
+	if (in_state(SELECT_DR_SCAN | SELECT_IR_SCAN))
+	{
+		CLRTMS;
+		jtag_ticktock();
+		jtag_state <<= 1; //CAPTURE_DR or CAPTURE_IR
+	}
+
+	if (in_state(CAPTURE_DR | CAPTURE_IR))
+	{
+		SETTMS;
+		jtag_ticktock();
+		jtag_state <<= 2; //EXIT1_DR or EXIT1_IR
+	}
+
+	if (in_state(SHIFT_DR | SHIFT_IR))
+	{
+		SETTMS;
+		jtag_ticktock();
+		jtag_state <<= 1; //EXIT1_DR or EXIT1_IR
+	}
+
+	if (in_state(EXIT1_DR | EXIT1_IR))
+	{
+		SETTMS;
+		jtag_ticktock();
+		jtag_state <<=3; //UPDATE_DR or UPDATE_IR
+	}
+
+	if (in_state(PAUSE_DR | PAUSE_IR))
+	{
+		SETTMS;
+		jtag_ticktock();
+		jtag_state <<= 1; // EXIT2_DR or EXIT2_IR
+	}
+
+	if (in_state(EXIT2_DR | EXIT2_IR))
+	{
+		SETTMS;
+		jtag_ticktock();
+		jtag_state <<= 1; // UPDATE_DR or UPDATE_IR
+	}
+
+	if (in_state(UPDATE_DR | UPDATE_IR | TEST_LOGIC_RESET))
+	{
+		CLRTMS;
+		jtag_ticktock();
+		jtag_state = RUN_TEST_IDLE;
+	}
+}
+
+//	NOTE: important: THIS MODULE REVOLVES AROUND RETURNING TO RUNTEST/IDLE, OR 
+//	THE FUNCTIONAL EQUIVALENT
+//! Shift N bits over TDI/TDO.	May choose LSB or MSB, and select whether to 
+//	terminate (TMS-high on last bit) and whether to return to RUNTEST/IDLE
+//		flags should be 0 for most uses.  
+//		for the extreme case, flags should be  (NOEND|NORETDLE|LSB)
+//		other edge cases can involve a combination of those three flags
+//
+//		the max bit-size that can be be shifted is 32-bits.  
+//		for longer shifts, use the NOEND flag (which infers NORETIDLE so the 
+//		additional flag is unnecessary)
+//
+//		NORETIDLE is used for special cases where (as with arm) the debug 
+//		subsystem does not want to return to the RUN-TEST/IDLE state between 
+//		setting IR and DR
+uint32_t jtag_trans_n(uint32_t word, 
+		      uint8_t bitcount, 
+		      enum eTransFlags flags) 
+{
+	uint8_t bit;
+	uint32_t high = (1L << (bitcount - 1));
+	uint32_t mask = high - 1;
+
+	if (!in_state(SHIFT_IR | SHIFT_DR))
+	{
+		// debugstr("jtag_trans_n from invalid TAP state");
+		return 0;
+	}
+
+	SAVETCLK;
+
+	if (flags & LSB) 
+	{
+		for (bit = bitcount; bit > 0; bit--) 
+		{
+			/* write MOSI on trailing edge of previous clock */
+			if (word & 1)
+			{
+				SETTDI;
+			}
+			else
+			{
+				CLRTDI;
+			}
+			word >>= 1;
+
+			if ((bit == 1) && !(flags & NOEND))
+				SETTMS; //TMS high on last bit to exit.
+
+			jtag_ticktock();
+
+			if ((bit == 1) && !(flags & NOEND))
+				jtag_state <<= 1; // Exit1-DR or Exit1-IR
+
+			/* read MISO on trailing edge */
+			if (READTDO)
+			{
+				word += (high);
+			}
+		}
+	} 
+	else 
+	{
+		for (bit = bitcount; bit > 0; bit--) 
+		{
+			/* write MOSI on trailing edge of previous clock */
+			if (word & high)
+			{
+				SETTDI;
+			}
+			else
+			{
+				CLRTDI;
+			}
+			word = (word & mask) << 1;
+
+			if ((bit==1) && !(flags & NOEND))
+				SETTMS; //TMS high on last bit to exit.
+
+			jtag_ticktock();
+
+			if ((bit == 1) && !(flags & NOEND))
+				jtag_state <<= 1; // Exit1-DR or Exit1-IR
+
+			/* read MISO on trailing edge */
+			word |= (READTDO);
+		}
+	}
+	
+	//This is needed for 20-bit MSP430 chips.
+	//Might break another 20-bit chip, if one exists.
+	if(bitcount==20){
+	  word = ((word << 16) | (word >> 4)) & 0x000FFFFF;
+	}
+	
+	RESTORETCLK;
+
+	if (!(flags & NOEND))
+	{
+		// exit state
+		jtag_ticktock();
+
+		jtag_state <<= 3; // Update-DR or Update-IR
+
+		// update state
+		if (!(flags & NORETIDLE))
+		{
+			CLRTMS;
+			jtag_ticktock();
+
+			jtag_state = RUN_TEST_IDLE;
+		}
+	}
+
+	return word;
+}
+
+//! Detects the width of the IR register
+uint16_t jtag_detect_ir_width()
+{
+	int i;
+	uint16_t width = 0;
+
+	if (!in_run_test_idle())
+	{
+		// debugstr("Not in run-test-idle state");
+		return 0;
+	}
+
+	// get to shift-ir state
+	jtag_capture_ir();
+	jtag_shift_register();
+
+	// first we shift in 1024 zeros
+	CLRTDI;
+	for(i = 0; i < 1024; i++)
+	{
+		jtag_ticktock();
+	}
+
+	// now we'll clock in ones until we see one
+	SETTDI;
+	for(i = 0; i < 1024; i++)
+	{
+		jtag_ticktock();
+		if(READTDO)
+			break;
+		width++;
+	}
+
+	// now get back to run-test-idle
+	jtag_run_test_idle();
+
+	return width;
+}
+
+//! Detects how many TAPs are in the JTAG chain
+uint16_t jtag_detect_chain_length()
+{
+	int i;
+	int bit;
+	uint16_t length = 0;
+
+	if (!in_run_test_idle())
+	{
+		// debugstr("detect chain length must start from run-test-idle");
+		return 0;
+	}
+
+	// The standard JTAG instruction for "bypass" mode is to set all 1's in the
+	// instruction register.  When in "bypass" mode, the DR acts like a 1-bit
+	// shift regiser.  So we can use that to detect how many TAPs are in the 
+	// chain.
+	
+	// first get into shift IR mode
+	jtag_capture_ir();
+	jtag_shift_register();
+
+	// then we flood the IR chain with all 1's to put all device's DRs
+	// into bypass mode
+	CLRTMS;
+	SETTDI;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i == 1023)
+			SETTMS; // exit on last bit
+		jtag_ticktock();
+	}
+	jtag_state = EXIT1_IR;
+
+	// go to Update-IR
+	CLRTDI;
+	jtag_ticktock();
+	jtag_state = UPDATE_IR;
+
+	// go to Shift-DR state
+	jtag_capture_dr();
+	jtag_shift_register();
+
+	// flush the DR's with zeros
+	CLRTMS;
+	CLRTDI;
+	for (i = 0; i < 1024; i++)
+	{
+		jtag_ticktock();
+	}
+
+	// send 1's into the DRs until we see one come out the other side
+	SETTDI;
+	for (i = 0; i < 1024; i++)
+	{
+		jtag_ticktock();
+		bit = READTDO;
+		if (bit)
+			break;
+		length++;
+	}
+
+	// now get back to run-test-idle
+	jtag_run_test_idle();
+
+	return length;
+}
+
+//! Gets device ID for specified chip in the chain
+uint32_t jtag_get_device_id(int chip)
+{
+	int i;
+	uint16_t chain_length;
+	uint32_t id = 0;
+
+	// reset everything
+	jtag_reset_tap();
+
+	// figure out how many devices are in the chain
+	chain_length = jtag_detect_chain_length();
+
+	if (chip >= chain_length)
+	{
+		// debugstr("invalid part index");
+		return 0;
+	}
+
+	// reset everything again because going through test-logic-reset forces
+	// all IR's to have their manufacturer specific instruction for IDCODE
+	// and loads the DR's with the chip's ID code.
+	jtag_reset_tap();
+
+	// get into shift DR state
+	jtag_capture_dr();
+	jtag_shift_register();
+
+	// read out the 32-bit ID codes for each device
+	CLRTMS;
+	CLRTDI;
+	for (i = 0; i < (chip + 1); i++)
+	{
+		id = jtag_trans_n(0xFFFFFFFF, 32, LSB | NOEND | NORETIDLE);
+	}
+
+	jtag_run_test_idle();
+
+	return id;
+}
+
+//! Shift 8 bits in/out of selected register
+uint8_t jtag_trans_8(uint8_t in)
+{
+	uint32_t out = jtag_trans_n((uint32_t)in, 8, MSB);
+	return (uint8_t)(0x000000FF & out);
+}
+
+//! Shift 16 bits in/out of selected register
+uint16_t jtag_trans_16(uint16_t in)
+{
+	uint32_t out = jtag_trans_n((uint32_t)in, 16, MSB);
+	return (uint16_t)(0x0000FFFF & out);
+}
+
+//! Shift 8 bits of the IR.
+uint8_t jtag_ir_shift_8(uint8_t in)
+{
+  /* Huseby's code, which breaks MSP430 support.
+     The code is broken because either the invalid jtag state error
+     causes the client to give up, or because it adds an extra clock edge.
+     
+	if (!in_run_test_idle())
+	{
+		debugstr("Not in run-test-idle state");
+		return 0;
+	}
+
+	// get intot the right state
+	jtag_capture_ir();
+	jtag_shift_register();
+  */
+  // idle
+  SETTMS;
+  jtag_ticktock();
+  // select DR
+  jtag_ticktock();
+  // select IR
+  CLRTMS;
+  jtag_ticktock();
+  // capture IR
+  jtag_ticktock();
+  //jtag_state = CAPTURE_IR;
+  jtag_state = SHIFT_IR;
+  // shift IR bits
+  return jtag_trans_8(in);
+}
+
+//! Shift 16 bits of the DR.
+uint16_t jtag_dr_shift_16(uint16_t in)
+{
+
+	if (!in_run_test_idle())
+	{
+		// debugstr("Not in run-test-idle state");
+		return 0;
+	}
+
+	// get intot the right state
+	jtag_capture_dr();
+	jtag_shift_register();
+
+	// shift DR, then idle
+	return jtag_trans_16(in);
+}
---
+++ firmware/common/jtag.h
@@ -0,0 +1,95 @@
+/*
+ * This file is part of GreatFET
+ * derived from GoodFET (thanks Travis)
+ */
+
+#ifndef __JTAG_H__
+#define __JTAG_H__
+
+#include <stdint.h>
+
+#define JTAG 0x10
+
+// All states in the JTAG TAP
+enum eTAPState
+{
+	UNKNOWN				= 0x0000,
+	TEST_LOGIC_RESET	= 0x0001,
+	RUN_TEST_IDLE		= 0x0002,
+	SELECT_DR_SCAN		= 0x0004,
+	CAPTURE_DR			= 0x0008,
+	SHIFT_DR			= 0x0010,
+	EXIT1_DR			= 0x0020,
+	PAUSE_DR			= 0x0040,
+	EXIT2_DR			= 0x0080,
+	UPDATE_DR			= 0x0100,
+	SELECT_IR_SCAN		= 0x0200,
+	CAPTURE_IR			= 0x0400,
+	SHIFT_IR			= 0x0800,
+	EXIT1_IR			= 0x1000,
+	PAUSE_IR			= 0x2000,
+	EXIT2_IR			= 0x4000,
+	UPDATE_IR			= 0x8000
+};
+
+extern unsigned char jtagid;
+
+//! the global state of the JTAG TAP
+extern enum eTAPState jtag_state;
+
+//! Returns true if we're in any of the data register states
+int in_dr();
+//! Returns true if we're in any of the instruction register states
+int in_ir();
+//! Returns true if we're in run-test-idle state
+int in_run_test_idle();
+//! Check the state
+int in_state(enum eTAPState state);
+
+//! jtag_trans_n flags
+enum eTransFlags
+{
+	MSB					= 0x0,
+	LSB					= 0x1,
+	NOEND				= 0x2,
+	NORETIDLE			= 0x4
+};
+
+//! Shift n bytes.
+uint32_t jtag_trans_n(uint32_t word, 
+					  uint8_t bitcount, 
+					  enum eTransFlags flags);
+//! Shift 8 bits in/out of selected register
+uint8_t jtag_trans_8(uint8_t in);
+//! Shift 16 bits in/out of selected register
+uint16_t jtag_trans_16(uint16_t in);
+//! Shift 8 bits of the IR.
+uint8_t jtag_ir_shift_8(uint8_t in);
+//! Shift 16 bits of the DR.
+uint16_t jtag_dr_shift_16(uint16_t in);
+//! Stop JTAG, release pins
+void jtag_stop(void);
+//! Setup the JTAG pin directions.
+void jtag_setup(void);
+//! Ratchet Clock Down and Up
+void jtag_ticktock();
+//! Reset the target device
+void jtag_reset_target();
+//! TAP RESET
+void jtag_reset_tap();
+//! Get into the Shift-IR or Shift-DR
+void jtag_shift_register();
+//! Get into Capture-IR state
+void jtag_capture_ir();
+//! Get into Capture-DR state
+void jtag_capture_dr();
+//! Get to Run-Test-Idle without going through Test-Logic-Reset
+void jtag_run_test_idle();
+//! Detect instruction register width
+uint16_t jtag_detect_ir_width();
+//! Detects how many TAPs are in the JTAG chain
+uint16_t jtag_detect_chain_length();
+//! Gets device ID for specified chip in the chain
+uint32_t jtag_get_device_id(int chip);
+
+#endif /*__JTAG_H__*/
