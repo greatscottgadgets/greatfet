a80cb710260a2d4a183b811f746b37305590a8b6
GravesJake <jacob@greatscottgadgets.com>
2018-07-25 13:48:01 -0600

    Fix license

--- firmware/common/adc.h
+++ firmware/common/adc.h
@@ -1,34 +1,5 @@
-/** @defgroup adc_defines ADC Defines
-
-@brief <b>Defined Constants and Types for the LPC43xx A/D Converter</b>
-
-@ingroup LPC43xx_defines
-
-@version 1.0.0
-
-@author @htmlonly &copy; @endhtmlonly 2012 Michael Ossmann <mike@ossmann.com>
-
-@date 10 March 2013
-
-LGPL License Terms @ref lgpl_license
- */
 /*
- * This file is part of the libopencm3 project.
- *
- * Copyright (C) 2012 Michael Ossmann <mike@ossmann.com>
- *
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * This file is part of GreatFET
  */
 
 #ifndef LPC43XX_ADC_H
