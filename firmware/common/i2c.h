a218b48003b63a0e8d84b20eb2c9f1498e6303ac
GravesJake <jacob@greatscottgadgets.com>
2018-07-25 17:28:58 -0600

    Fix license

--- firmware/common/i2c.h
+++ firmware/common/i2c.h
@@ -1,38 +1,5 @@
-/** @defgroup i2c_defines I2C Defines
-
-@brief <b>Defined Constants and Types for the LPC43xx I2C</b>
-
-@ingroup LPC43xx_defines
-
-@version 1.0.0
-
-@author @htmlonly &copy; @endhtmlonly 2012 Michael Ossmann <mike@ossmann.com>
-@author @htmlonly &copy; @endhtmlonly 2013 Benjamin Vernoux <bvernoux@gmail.com>
-@author @htmlonly &copy; @endhtmlonly 2014 Jared Boone <jared@sharebrained.com>
-
-@date 19 December 2013
-
-LGPL License Terms @ref lgpl_license
- */
 /*
- * This file is part of the libopencm3 project.
- *
- * Copyright (C) 2012 Michael Ossmann <mike@ossmann.com>
- * Copyright (C) 2013 Benjamin Vernoux <bvernoux@gmail.com>
- * Copyright (C) 2014 Jared Boone <jared@sharebrained.com>
- *
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ * This file is part of GreatFET
  */
 
 #ifndef LPC43XX_I2C_H
