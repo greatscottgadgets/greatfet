a41076763e01437c97cef8a35dc3bfcb9069dcdd
Dominic Spill <dominicgs@gmail.com>
2018-08-06 13:20:33 -0600

    Attempt to clean up DMA code

--- firmware/greatfet_usb/usb_api_operacake.c
+++ firmware/greatfet_usb/usb_api_operacake.c
@@ -15,77 +15,6 @@
 static uint32_t tx_samplerate = 100000000;
 volatile bool operacake_tx_enabled = false;
 
-void operacake_dma_isr() {
-	// gpio_dma_irq_tc_acknowledge();
-	// Experimental
-	GPDMA_INTTCCLEAR = GPDMA_INTTCCLEAR_INTTCCLEAR(0x20);
-	led_toggle(LED3);
-	led_toggle(LED4);
-}
-
-#define TIMER_CLK_SPEED 204000000
-#define TIMER_PRESCALER 0
-#define LLI_COUNT 4
-#define SYMBOL_BUFFER_SIZE 32
-
-gpdma_lli_t oc_dma_lli[LLI_COUNT];
-
-uint32_t symbol_buffer[] = {
-	0xFFFFFFFF, 0x00, 0xFFFFFFFF, 0x00,
-	0xFFFFFFFF, 0x00, 0xFFFFFFFF, 0x00,
-	0xFFFFFFFF, 0x00, 0xFFFFFFFF, 0x00,
-	0xFFFFFFFF, 0x00, 0xFFFFFFFF, 0x00,
-	0xFFFFFFFF, 0x00, 0xFFFFFFFF, 0x00,
-	0xFFFFFFFF, 0x00, 0xFFFFFFFF, 0x00,
-	0xFFFFFFFF, 0x00, 0xFFFFFFFF, 0x00,
-	0xFFFFFFFF, 0x00, 0xFFFFFFFF, 0x00,
-};
-
-void operacake_tx_stop(void) {
-	timer_disable_counter(TIMER1);  /* Disable timers */
-	timer_disable_counter(TIMER2);
-	nvic_disable_irq(NVIC_DMA_IRQ); /* Disable DMA interrupt */
-	gpio_dma_stop();                /* Disable DMA config */
-	gpio_dma_irq_tc_acknowledge();
-	gpio_dma_irq_err_clear();
-}
-
-void operacake_tx_start(void) {
-	/* Make sure nothing is running before we configure it */
-	operacake_tx_stop();
-	led_toggle(LED4);
-
-	vector_table.irq[NVIC_DMA_IRQ] = operacake_dma_isr;
-	nvic_set_priority(NVIC_DMA_IRQ, 0);
-	nvic_enable_irq(NVIC_DMA_IRQ);
-	/*
-	 * TIMER1 produces the TX DAC clock signal
-	 * TIMER2 is derived from TIMER1 and triggers DAC data signals
-	 */
-	TIMER1_MCR = TIMER_MCR_MR0R;
-	TIMER1_MR0 = ((TIMER_CLK_SPEED / (2*(TIMER_PRESCALER + 1))) / tx_samplerate) - 1;
-	TIMER1_MR3 = TIMER1_MR0;
-	TIMER1_EMR = (TIMER_EMR_EMC_TOGGLE << TIMER_EMR_EMC0_SHIFT) | (TIMER_EMR_EMC_TOGGLE << TIMER_EMR_EMC3_SHIFT);
-	timer_set_prescaler(TIMER1, TIMER_PRESCALER);
-	timer_set_mode(TIMER1, TIMER_CTCR_MODE_TIMER);
-	timer_reset(TIMER1);
-	/* T1_MAT3 is connected to T2_CAP3 through the GIMA by default */
-	TIMER2_MCR = (TIMER_MCR_MR0I | TIMER_MCR_MR0R);
-	TIMER2_MR0 = 2;
-	TIMER2_CCR = 0;
-	timer_set_prescaler(TIMER2, TIMER_PRESCALER);
-	timer_set_mode(TIMER2, (TIMER_CTCR_MODE_COUNTER_BOTH | TIMER_CTCR_CINSEL_CAPN_3));
-	timer_reset(TIMER2);
-
-	void* const target = (void*)&(GPIO_LPC_PORT(2)->pin);
-	gpio_dma_config_lli(oc_dma_lli, LLI_COUNT, symbol_buffer, target, SYMBOL_BUFFER_SIZE>>1);
-	gpio_dma_tx_start(oc_dma_lli);
-	gpio_dma_init();
-	timer_enable_counter(TIMER2);
-	timer_enable_counter(TIMER1);
-}
-
-
 #define OPERACAKE_GPIO_U2CTRL1(x) (x<<6)
 #define OPERACAKE_GPIO_U2CTRL0(x) (x<<3)
 #define OPERACAKE_GPIO_U3CTRL1(x) (x<<2)
@@ -111,7 +40,6 @@ void operacake_tx_mode(void)
 {
 	volatile uint32_t* mask = &(GPIO_LPC_PORT(2)->mask);
 	/* 1 = masked, 0 = set via mpin */
-	// *mask = 0xFFFFFFA3;
 	*mask = ~(OPERACAKE_GPIO_U2CTRL0(1) | OPERACAKE_GPIO_U2CTRL1(1) | OPERACAKE_GPIO_U3CTRL0(1) | OPERACAKE_GPIO_U3CTRL1(1));
 	volatile uint32_t* target = &(GPIO_LPC_PORT(2)->mpin);
 	while(1) {
@@ -120,6 +48,69 @@ void operacake_tx_mode(void)
 		*target = PATH3;
 		delay(100);
 	}
+}
+
+void operacake_dma_isr() {
+	// gpio_dma_irq_tc_acknowledge();
+	// Experimental
+	GPDMA_INTTCCLEAR = GPDMA_INTTCCLEAR_INTTCCLEAR(0x20);
+	led_toggle(LED3);
+	led_toggle(LED4);
+}
+
+#define TIMER_CLK_SPEED 204000000
+#define TIMER_PRESCALER 0
+#define LLI_COUNT 1
+#define SYMBOL_BUFFER_SIZE 32
+
+gpdma_lli_t oc_dma_lli[LLI_COUNT];
+
+uint8_t symbol_buffer[] = {
+	PATH1, PATH3, PATH1, PATH3,
+	PATH1, PATH3, PATH1, PATH3,
+	PATH1, PATH3, PATH1, PATH3,
+	PATH1, PATH3, PATH1, PATH3,
+	PATH1, PATH3, PATH1, PATH3,
+	PATH1, PATH3, PATH1, PATH3,
+	PATH1, PATH3, PATH1, PATH3,
+	PATH1, PATH3, PATH1, PATH3,
+};
+
+void operacake_tx_stop(void) {
+	timer_disable_counter(TIMER2);  /* Disable timers */
+	nvic_disable_irq(NVIC_DMA_IRQ); /* Disable DMA interrupt */
+	gpio_dma_stop();                /* Disable DMA config */
+	gpio_dma_irq_tc_acknowledge();
+	gpio_dma_irq_err_clear();
+}
+
+void operacake_tx_start(void) {
+	/* Make sure nothing is running before we configure it */	
+	operacake_tx_stop();
+	led_toggle(LED4);
+
+	vector_table.irq[NVIC_DMA_IRQ] = operacake_dma_isr;
+	nvic_set_priority(NVIC_DMA_IRQ, 0);
+	nvic_enable_irq(NVIC_DMA_IRQ);
+	/*
+	 * TIMER2 is used to control DMA transfers to GPIO pins
+	 */
+
+	TIMER2_MCR = (TIMER_MCR_MR0I | TIMER_MCR_MR0R);
+	TIMER2_MR0 = ((TIMER_CLK_SPEED / (2*(TIMER_PRESCALER + 1))) / tx_samplerate) - 1;
+	timer_set_prescaler(TIMER2, TIMER_PRESCALER);
+	timer_set_mode(TIMER2, TIMER_CTCR_MODE_TIMER);
+	timer_reset(TIMER2);
+
+	volatile uint32_t* mask = &(GPIO_LPC_PORT(2)->mask);
+	/* 1 = masked, 0 = set via mpin */
+	*mask = ~(OPERACAKE_GPIO_U2CTRL0(1) | OPERACAKE_GPIO_U2CTRL1(1) | OPERACAKE_GPIO_U3CTRL0(1) | OPERACAKE_GPIO_U3CTRL1(1));
+
+	void* const target = (void*)&(GPIO_LPC_PORT(2)->mpin);
+	gpio_dma_config_lli(oc_dma_lli, LLI_COUNT, symbol_buffer, target, SYMBOL_BUFFER_SIZE>>1);
+	gpio_dma_tx_start(oc_dma_lli);
+	gpio_dma_init();
+	timer_enable_counter(TIMER2);
 }
 
 usb_request_status_t usb_vendor_request_operacake(
