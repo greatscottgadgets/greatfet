ac02195ed4557f471978d88ea86446e58a0729ab
GravesJake <jacob@greatscottgadgets.com>
2018-09-19 18:02:51 -0600

    Modify single address transmission arguments to allow for more flexibility in user input

--- host/greatfet/commands/greatfet_i2c.py
+++ host/greatfet/commands/greatfet_i2c.py
@@ -21,8 +21,10 @@ def main():
     parser.add_argument('-s', dest='serial', metavar='<serialnumber>', type=str,
                         help="Serial number of device, if multiple devices", default=None)
     parser.add_argument('-v', dest='verbose', action='store_true', help="Write data from file")
-    parser.add_argument('-a', '--address_scan', action='store_true', help="Scan all possible i2c addresses")
-    parser.add_argument('-t', '--transmit', nargs=3, metavar=('address', 'data', 'receive_length'), help="Transmit data over the I2C Bus") # dest='address', type=str, help="Transmit data with the given address")
+    parser.add_argument('-z', '--scan', action='store_true', help="Scan all possible i2c addresses")
+    parser.add_argument('-a', '--address', nargs=1, type=str, help="Address to transmit data to over the I2C Bus") 
+    parser.add_argument('-t', '--transmit', metavar='data', default=[], help="Data to transmit over the I2C Bus")
+    parser.add_argument('-r', '--receive_length', default=1, help="Number of bytes expecting to receive from the I2C Bus") # dest='address', type=str, help="Transmit data with the given address")
     args = parser.parse_args()
 
     log_function = log_verbose if args.verbose else log_silent
@@ -38,10 +40,11 @@ def main():
             print("No GreatFET board found!", file=sys.stderr)
         sys.exit(errno.ENODEV)
 
-    if args.address_scan:
+    if args.scan:
         scan(device)
-    if args.transmit:
-        transmit(device, args.transmit)
+    if args.address:
+        print("address: ", args.address)
+        transmit(device, args.address[0], args.transmit[0], int(args.receive_length[0]))
 
 
 def scan(device):
@@ -50,11 +53,8 @@ def scan(device):
     print("Working address(es): ", valid_addresses)
 
 
-def transmit(device, transmit_info):
+def transmit(device, address, data, receive_length):
     received_data = []
-    address = transmit_info[0]
-    data = transmit_info[1]
-    receive_length = int(transmit_info[2])
     i2c_device = I2CDevice(device.i2c, int(address, 16)>>1)
     received_data.append(i2c_device.transmit(data, receive_length))
     print(received_data)
