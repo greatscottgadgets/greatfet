1402738db81eeedff1ee30d7ad78efe256f223d1
Kate J. Temkin <k@ktemkin.com>
2018-11-09 11:24:23 -0700

    host tools: allow the 'greatfet' tool to go by 'gf' as well
    This is how I have my local setup working; and it feels _great_.

--- host/setup.py
+++ host/setup.py
@@ -14,6 +14,7 @@ setup(
     entry_points={
         'console_scripts': [
             'greatfet = greatfet.commands.greatfet:main',
+            'gf = greatfet.commands.greatfet:main',
             'greatfet_adc = greatfet.commands.greatfet_adc:main',
             'greatfet_adf7242 = greatfet.commands.greatfet_adf7242:main',
             'greatfet_firmware = greatfet.commands.greatfet_firmware:main',
