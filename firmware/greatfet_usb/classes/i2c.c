2fd8375e8e704b1c20e7a542d4d597c4f8664c01
GravesJake <jacob@greatscottgadgets.com>
2018-12-21 14:59:43 -0700

    i2c: convert to new comms API

---
+++ firmware/greatfet_usb/classes/i2c.c
@@ -0,0 +1,80 @@
+/*
+ * This file is part of GreatFET
+ */
+
+#include <drivers/usb/lpc43xx/usb_queue.h>
+
+#include <stddef.h>
+#include <greatfet_core.h>
+#include <i2c_bus.h>
+#include <i2c.h>
+#include <errno.h>
+#include <drivers/comms.h>
+
+
+#define CLASS_NUMBER_SELF (0x108)
+
+static uint16_t duty_cycle_count;
+static uint8_t status;
+
+static int i2c_verb_start(struct command_transaction *trans)
+{
+	uint16_t value = comms_argument_parse_uint16_t(trans);
+	if (value == 0) {
+		duty_cycle_count = 255;
+	} else {
+		duty_cycle_count = value;
+	}
+	i2c_bus_start(&i2c0, duty_cycle_count);
+
+	return 0;
+}
+
+static int i2c_verb_stop()
+{
+	i2c_bus_stop(&i2c0);
+
+	return 0;	
+}
+
+static int i2c_verb_xfer(struct command_transaction *trans)
+{
+	uint32_t tx_length;
+	uint16_t address 		= comms_argument_parse_uint16_t(trans);
+	uint16_t rx_length 		= comms_argument_parse_uint16_t(trans);
+	uint8_t *data_to_write 	= comms_argument_read_buffer(trans, -1, &tx_length);
+	uint8_t *i2c_rx_buffer 	= comms_response_reserve_space(trans, rx_length);
+	// TODO: data validation
+
+	if (!comms_transaction_okay(trans)) {
+        return EBADMSG;
+    }
+
+	status = i2c_bus_transfer(&i2c0, address & 0xff, data_to_write, tx_length, 
+					 i2c_rx_buffer, rx_length);
+	comms_response_add_uint8_t(trans, status);
+
+	return 0;
+}
+
+/**
+ * Verbs for the firmware API.
+ */
+static struct comms_verb _verbs[] = {
+		{ .name = "start", .handler = i2c_verb_start,
+			.in_signature = "<I", .out_signature = "",
+			.in_param_names = "value, duty_cycle_count",
+			.doc = "Initialize and transmit a start bit to an I2C device" },
+		{ .name = "stop", .handler = i2c_verb_stop,
+			.in_signature = "", .out_signature = "",
+			.in_param_names = "",
+			.doc = "Transmit a stop bit to an I2C device" },
+		{ .name = "xfer", .handler = i2c_verb_xfer,
+			.in_signature = "<HH*X", .out_signature = "<*B",
+			.in_param_names = "value, index, data", .out_param_names = "response, status",
+			.doc = "Transmits data over the I2C bus and responds accordingly" },
+		{} // Sentinel
+};
+COMMS_DEFINE_SIMPLE_CLASS(i2c, CLASS_NUMBER_SELF, "i2c", _verbs,
+		"API for I2C communication.");
+
--- host/greatfet/commands/greatfet_i2c.py
+++ host/greatfet/commands/greatfet_i2c.py
@@ -4,9 +4,9 @@
 
 from __future__ import print_function
 
-import argparse
 import errno
 import sys
+import ast
 
 import greatfet
 from greatfet import GreatFET
@@ -16,15 +16,14 @@ from greatfet.peripherals.i2c_bus import I2CBus
 
 
 def main():
+    from greatfet.utils import GreatFETArgumentParser
+
     # Set up a simple argument parser.
-    parser = argparse.ArgumentParser(description="Utility for I2C communication via GreatFET")
-    parser.add_argument('-s', dest='serial', metavar='<serialnumber>', type=str,
-                        help="Serial number of device, if multiple devices", default=None)
-    parser.add_argument('-v', dest='verbose', action='store_true', help="Write data from file")
+    parser = GreatFETArgumentParser(description="Utility for I2C communication via GreatFET")
     parser.add_argument('-z', '--scan', action='store_true', help="Scan all possible i2c addresses")
-    parser.add_argument('-a', '--address', nargs=1, type=str, help="Address to transmit data to over the I2C Bus") 
-    parser.add_argument('-t', '--transmit', metavar='data', default=[], help="Data to transmit over the I2C Bus")
-    parser.add_argument('-r', '--receive_length', default=1, help="Number of bytes expecting to receive from the I2C Bus")
+    parser.add_argument('transmit', nargs='*', type=ast.literal_eval, default=[], help="Data to transmit over the I2C Bus <byte byte byte..>")
+    parser.add_argument('-a', '--address', nargs=1, type=ast.literal_eval, help="Address to transmit data to over the I2C Bus") 
+    parser.add_argument('-r', '--receive_length', default=0, help="Number of bytes expecting to receive from the I2C Bus")
     args = parser.parse_args()
 
     log_function = log_verbose if args.verbose else log_silent
@@ -55,11 +54,14 @@ def scan(device, log_function):
     
 
 def transmit(device, address, data, receive_length, log_function):
-    i2c_device = I2CDevice(device.i2c, int(address, 16)>>1)
-    received_data = i2c_device.transmit(data, receive_length)
-    log_function("received bytes:")
-    for byte in received_data:
-        log_function(hex(byte))
+    i2c_device = I2CDevice(device.i2c, address >> 1)
+    received_data, i2c_status = i2c_device.transmit(data, receive_length)
+    if received_data:
+        log_function("received bytes:")
+        for byte in received_data:
+            log_function(hex(byte))
+    log_function("i2c status:")
+    log_function(hex(i2c_status))
 
 
 if __name__ == '__main__':
