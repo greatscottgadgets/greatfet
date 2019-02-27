449de34445e1d269fb8d411735e8010915d3e5dd
Dominic Spill <dominicgs@gmail.com>
2019-02-27 14:50:43 +0000

    It wouldn't be a new feature if I didn't forget a header file

---
+++ firmware/greatfet_usb/classes/operacake.h
@@ -0,0 +1,15 @@
+/*
+ * This file is part of GreatFET
+ */
+
+#ifndef __CLASS_OPERACAKE_H__
+#define __CLASS_OPERACAKE_H__
+
+/**
+ * Performs a single unit of operacake PSK mode's work.
+ * To be honest this is an  infinite loop, which is bad,
+ * but it'sa PoC.
+ */
+void service_operacake(void);
+
+#endif /* __CLASS_OPERACAKE_H__ */
