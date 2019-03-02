09c8b8e04db579c3b7716246a7cb256f27a0f910
Kate J. Temkin <k@ktemkin.com>
2019-03-01 21:49:33 -0700

    firmware: fix builds for XPLORER / RAD1O (and spell rad1o correctly)

--- firmware/boards/rad1o_badge/board.cmake
+++ firmware/boards/rad1o_badge/board.cmake
@@ -6,7 +6,7 @@ include_guard()
 
 # General board information.
 set(BOARD_ID    2)
-set(BOARD_NAME  "CCC rad10-badge")
+set(BOARD_NAME  "CCC rad1o-badge")
 
 # Pull in the common code for the LPC4330.
 include(${PATH_LIBGREAT_FIRMWARE_CMAKE}/platform/lpc4330.cmake)
