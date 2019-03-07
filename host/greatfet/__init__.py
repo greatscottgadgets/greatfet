9513c393ee641fb043d1ebd7d2e4645ef26ccd1d
Kate J. Temkin <k@ktemkin.com>
2019-03-07 15:28:27 -0700

    host: allow the DFU stub to be included in a given distribution

--- host/greatfet/__init__.py
+++ host/greatfet/__init__.py
@@ -1,6 +1,12 @@
 from __future__ import print_function
-
 # Alias objects to make them easier to import.
 from .greatfet import GreatFET
-
 GreatFET = GreatFET  # pyflakes
+
+def greatfet_assets_directory():
+    """ Provide a quick function that helps us get at our assets directory. """
+    import os
+
+    # Find the path to the module, and then find its assets folder.
+    module_path = os.path.dirname(__file__)
+    return os.path.join(module_path, 'assets')
