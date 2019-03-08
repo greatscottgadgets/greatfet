5a433d79467e689118ef96ba5809d7925b3cbf72
Kate J. Temkin <k@ktemkin.com>
2019-03-07 17:09:21 -0700

    build: make it easy to include firmware in our host distributions
    Fixes #208.

--- host/greatfet/__init__.py
+++ host/greatfet/__init__.py
@@ -10,3 +10,15 @@ def greatfet_assets_directory():
     # Find the path to the module, and then find its assets folder.
     module_path = os.path.dirname(__file__)
     return os.path.join(module_path, 'assets')
+
+
+def find_greatfet_asset(filename):
+    """ Returns the path to a given GreatFET asset, if it exists, or None if the GreatFET asset isn't provided."""
+    import os
+
+    asset_path = os.path.join(greatfet_assets_directory(), filename)
+
+    if os.path.isfile(asset_path):
+        return asset_path
+    else:
+        return None
