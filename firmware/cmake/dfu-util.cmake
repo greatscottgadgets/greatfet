7dac730c7f1c4f7ce77661150c6bbdfad7261139
Kate J. Temkin <k@ktemkin.com>
2019-03-10 22:28:53 -0600

    build: correct logic for missing dfu-util

--- firmware/cmake/dfu-util.cmake
+++ firmware/cmake/dfu-util.cmake
@@ -2,6 +2,8 @@
 # This file is part of GreatFET
 #
 
+include_guard(GLOBAL)
+
 execute_process(
 	COMMAND dfu-suffix -V
 	WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
@@ -11,10 +13,16 @@ execute_process(
 	OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
-set(DFU_ALL "")
+# Depending on how well the DFU utility binary ran, either mark us as capable or incapable of building DFU targets.
 if(NOT DFU_NOT_FOUND)
-    set(DFU_ALL "ALL")
+	set(DFU_ALL "ALL" CACHE STRING "determines whether DFU variables will be included in the ALL target")
+	set(DFU_TARGETS_AVAILABLE "YES" CACHE BOOL "identifies whether DFU targets can be built")
 else(NOT DFU_NOT_FOUND)
-	MESSAGE(STATUS "dfu-suffix not found: not building DFU file")
+	message(WARNING "dfu-suffix not found: not building DFU targets")
+	set(DFU_ALL "" CACHE STRING "determines whether DFU variables will be included in the ALL target")
+	set(DFU_TARGETS_AVAILABLE "NO" CACHE BOOL "identifies whether DFU targets can be built")
 endif(NOT DFU_NOT_FOUND)
 
+# DFU state shouldn't normally show up in the CMake configuration.
+mark_as_advanced(DFU_ALL)
+mark_as_advanced(DFU_TARGETS_AVAILABLE)
