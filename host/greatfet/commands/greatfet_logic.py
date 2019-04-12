a15450a488a41c32a387fff676a86e7f548ae506
Kate J. Temkin <k@ktemkin.com>
2019-04-11 18:04:00 -0600

    host: logic: correct pulseview generation when given an absolute path

--- host/greatfet/commands/greatfet_logic.py
+++ host/greatfet/commands/greatfet_logic.py
@@ -21,12 +21,14 @@ def main():
     parser = argparse.ArgumentParser(description="Logic analyzer implementation for GreatFET")
     parser.add_argument('-s', dest='serial', metavar='<serialnumber>', type=str,
                         help="Serial number of device, if multiple devices", default=None)
-    parser.add_argument('-o', dest='output', metavar='<filename>', type=str,
-                        help="Write data to raw binary file")
-    parser.add_argument('-p', '--pulseview', action='store_true',
-                        help="Write data to pulseview compatible archive")
+    parser.add_argument('-o', '-b', '--binary', dest='binary', metavar='<filename>', type=str,
+                        help="Generate a binary file contianing the captured data.")
+    parser.add_argument('-p', '--pulseview', dest='pulseview', metavar="<filename>", type=str,
+                        help="Generate a PulseView session file.")
     parser.add_argument('-v', dest='verbose', action='store_true', help="Write data from file")
     args = parser.parse_args()
+
+    capture_data_name='logic-1'
 
     log_function = log_verbose if args.verbose else log_silent
 
@@ -44,51 +46,48 @@ def main():
     device.apis.logic_analyzer.start()
     time.sleep(1)
 
-    fd, path = tempfile.mkstemp(None, None, os.getcwd())
-    if args.output:
+    _, path = tempfile.mkstemp(None, None, os.getcwd())
+    if args.binary:
         bin_file_name = arc_name = args.output
         sr_name = args.output + ".sr"
     else:
         bin_file_name = path
-        arc_name = "logic-1"
-        sr_name = "pulseview_session.sr"
+
 
     print("Press Ctrl+C to stop reading data from device")
     try:
-        with open(bin_file_name, "wb") as bin_file, os.fdopen(fd, "wb") as tmp:
+        with open(bin_file_name, "wb") as bin_file:
             try:
                 while True:
                     d = device.comms.device.read(0x81, 16384, 1000)
-                    if args.output:
-                        bin_file.write(d)
-                    if args.pulseview:
-                        tmp.write(d)
+                    bin_file.write(d)
             except KeyboardInterrupt:
                 print()
 
-            if args.output:
+            if args.binary:
                 print("Binary data written to file '%s'" % args.output)
+
             if args.pulseview:
-                metadata_str = ("[device 1]\n"
-                                "capturefile=%s\n"
-                                "total probes=8\n"
-                                "samplerate=17 MHz\n"
-                                "total analog=0\n"
-                                "probe1=SGPIO0\n"
-                                "probe2=SGPIO1\n"
-                                "probe3=SGPIO2\n"
-                                "probe4=SGPIO3\n"
-                                "probe5=SGPIO4\n"
-                                "probe6=SGPIO5\n"
-                                "probe7=SGPIO6\n"
-                                "probe8=SGPIO7\n"
-                                "unitsize=1\n") % arc_name
+                metadata_str = "[device 1]\n" \
+                    "capturefile={}\n" \
+                    "total probes=8\n" \
+                    "samplerate=17 MHz\n" \
+                    "total analog=0\n" \
+                    "probe1=SGPIO0\n" \
+                    "probe2=SGPIO1\n" \
+                    "probe3=SGPIO2\n" \
+                    "probe4=SGPIO3\n" \
+                    "probe5=SGPIO4\n" \
+                    "probe6=SGPIO5\n" \
+                    "probe7=SGPIO6\n" \
+                    "probe8=SGPIO7\n" \
+                    "unitsize=1\n".format(capture_data_name)
                 # pulseview compatible .sr archive
-                with ZipFile(sr_name, "w") as zip:
-                    zip.write(bin_file_name, arc_name)
+                with ZipFile(args.pulseview, "w") as zip:
+                    zip.write(bin_file_name, arcname='logic-1')
                     zip.writestr("metadata", metadata_str)
                     zip.writestr("version", "2")
-                print("Pulseview compatible session file created: '%s'" % sr_name)
+                print("Pulseview compatible session file created: '%s'" % args.pulseview)
     finally:
         try:
             os.remove(path)
